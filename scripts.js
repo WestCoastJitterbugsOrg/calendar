59f189a04b7aae7e6d75936f8b2a44f802418888
Jean-Philippe Green <jp@jp-dekstop.jp>
2020-09-10 13:46:09 +0200

    Fetch courses from Calendar API

--- scripts.js
+++ scripts.js
@@ -4,34 +4,39 @@
    * https://developers.google.com/explorer-help/guides/code_samples#javascript
    */
 
-
-
-function authenticate() {
-    return gapi.auth2.getAuthInstance()
-        .signIn({scope: "https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/calendar.events.readonly https://www.googleapis.com/auth/calendar.readonly"})
-        .then(function() { console.log("Sign-in successful"); },
-              function(err) { console.error("Error signing in", err); });
-  }
   function loadClient() {
+    gapi.client.setApiKey("AIzaSyCMeXBPWfEvrxH4-U8y3VpWhDPZnwYqRMc");
     return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest")
         .then(function() { console.log("GAPI client loaded for API"); },
               function(err) { console.error("Error loading GAPI client for API", err); });
   }
-  // Make sure the client is loaded and sign-in is complete before calling this method.
+  // Make sure the client is loaded before calling this method.
   function execute() {
     return gapi.client.calendar.events.list({
       "calendarId": "wcj.se_57n2cj034c49cirl0rl20t3io4@group.calendar.google.com",
-      "showDeleted": false,
-      "timeZone": "2020-09-08"
+      "alwaysIncludeEmail": false,
+      "timeMin": "2020-09-10T10:43:14.507Z"
     })
         .then(function(response) {
                 // Handle the results here (response.result has the parsed body).
                 console.log("Response", response);
+                const summaries = response.result.items.map(x => x.summary);
+                var uniqueSummaries = [];
+                $.each(summaries, function(i, el){
+                    if($.inArray(el, uniqueSummaries) === -1) uniqueSummaries.push(el);
+                });
+
+                $.each(uniqueSummaries, function(i, el) {
+                  console.log(i);
+                  $("#courseList").append(`<div><input type="checkbox" id="course-summary-${i}"> <label for="course-summary-${i}">${el}</label></div>`)
+                })
               },
               function(err) { console.error("Execute error", err); });
   }
-  gapi.load("client:auth2", function() {
-    gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
-  });
 
-  
+
+  function handleClientLoad() {
+      gapi.load("client", () => {
+        loadClient().then(execute);
+      });
+  }
