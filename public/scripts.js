35c3773545622f5c8f819e25c06cb30bc168cd27
Jean-Philippe Green <jp@jp-dekstop.jp>
2020-09-13 23:21:59 +0200

    More responsive and better UI

--- public/scripts.js
+++ public/scripts.js
@@ -12,7 +12,7 @@ function execute() {
   return gapi.client.calendar.events.list({
     "calendarId": "wcj.se_57n2cj034c49cirl0rl20t3io4@group.calendar.google.com",
     "alwaysIncludeEmail": false,
-    "timeMin": "2020-09-10T10:43:14.507Z"
+    "timeMin": new Date().toISOString() // "2020-09-10T10:43:14.507Z"
   })
     .then(function (response) {
       console.log(response.result.items);
@@ -25,19 +25,31 @@ function execute() {
       });
 
       $.each(allUniqueEvents, function (i, el) {
-        const checkboxEl = $(`<input type="checkbox" class="courseCheckbox" id="course-${el.id}">`).change(reloadCalendar)
-        $("#courseList").append($(`<label>${el.title}</label>`).prepend(checkboxEl))
+        const id = `course-${el.id}`;
+        const checkboxEl = $(`<input type="checkbox" class="courseCheckbox" id="${id}">`).change(reloadCalendar);
+        const liContent = checkboxEl.add(`<label for="${id}">${el.title}</label>`);
+        $("#courseList").append($("<li></li>").append(liContent));
       });
 
       var calendarEl = $('#calendar').get(0);
       calendar = new FullCalendar.Calendar(calendarEl, {
+        headerToolbar: {center: 'dayGridMonth,timeGridWeek,listMonth,listWeek'},
+        nowIndicator: true,
+        buttonText: {
+          dayGridMonth: 'Month grid',
+          timeGridWeek: 'Week grid',
+          listMonth: 'Month list',
+          listWeek: 'Week list'
+        },
+        aspectRatio: 2,
         initialView: 'dayGridMonth',
         firstDay: 1, // Monday
         eventTimeFormat: {
           hour: '2-digit',
           minute: '2-digit',
           meridiem: false,
-          hour12: false
+          hour12: false,
+
         }
       });
       calendar.render();
