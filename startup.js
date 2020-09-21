61797ef04237122ba5b45b439785eddf4c86d264
Jean-Philippe Green <jp@jp-dekstop.jp>
2020-09-21 04:00:01 +0200

    Make work in node

--- startup.js
+++ startup.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
-app.use("/personal-calendar", express.static(path.join(__dirname, 'public')));
+app.use("/personal-calendar", express.static(path.join(__dirname, 'dist')));
 
 app.listen('8080');
