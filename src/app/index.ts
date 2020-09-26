0c929c53e5fb4019364f04d588294fb304debf3a
Jean-Philippe Green <jp@jp-dekstop.jp>
2020-09-26 14:44:25 +0200

    Setup ADA test framework

--- src/app/index.ts
+++ src/app/index.ts
@@ -7,7 +7,8 @@ const colorHash = colorHashFactory({ saturation: 0.35 }); // {/*hue: [150, 210],
 function handleClientLoad() {
   gapi.load("client", async () => {
     gapi.client.setApiKey("AIzaSyCMeXBPWfEvrxH4-U8y3VpWhDPZnwYqRMc");
-    await gapi.client.load("https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest")
+    // Sometimes Typescript won't find the load function with one argument, so I added undefined in the second
+    await gapi.client.load("https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest", undefined)
       .catch((err) => { alert("Error loading GAPI client for API: " + err); });
     // Load from Google calendar
     const response= await gapi.client.calendar.events.list({
