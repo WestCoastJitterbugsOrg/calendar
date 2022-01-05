0646754d34c1223ac403e0a2ac6247eec3164694
Jean-Philippe Green <jeanphilippe.green@protonmail.com>
2022-01-05 17:50:41 +0100

    Extremely failsafe, will never bubble up an exception

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,26 +1,35 @@
-import './index.css';
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+try {
+  const wcjCalElement = document.getElementById("wcjcal");
 
-const wcjCalElement = document.getElementById("wcjcal");
+  if (wcjCalElement == null) {
+    throw Error("Could not find #wcjcal element in DOM");
+  } else {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      wcjCalElement
+    );
 
-if (wcjCalElement == null) {
-  alert("Fatal: Could not find #wcjcal element in DOM");
-} else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    wcjCalElement
-  );
-
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  if (process.env.NODE_ENV === "development") {
-    console.log("reporting web vitals");
-    reportWebVitals(console.log);
+    // If you want to start measuring performance in your app, pass a function
+    // to log results (for example: reportWebVitals(console.log))
+    // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+    if (process.env.NODE_ENV === "development") {
+      console.log("reporting web vitals");
+      reportWebVitals(console.log);
+    }
   }
+} catch (error) {
+  console.error(
+    `
+  An error occured in WCJ Calendar!
+  Please contact it@wcj.se with the error message:
+  `,
+    error
+  );
 }
