734812b133c155ef15b6e999f7841aee2000d912
Jean-Philippe Green <jeanphilippe.green@protonmail.com>
2022-02-21 23:50:34 +0100

    Remove spinner.css

--- client/src/shared/Spinner.tsx
+++ client/src/shared/Spinner.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import "./spinner.css";
 
 export default function SpinLoader() {
   return (
     <div className="flex h-screen justify-center items-center bg-wcj-sand">
-      <div className="spinner"></div>
+      <div className="animate-spin h-16 w-16 rounded-[50%] border-8 border-solid border-t-wcj-coral border-r-wcj-cyan border-b-wcj-red border-l-wcj-mint"></div>
     </div>
   );
 }
