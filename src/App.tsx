f268659dfce687b473fbb20547182d2f39f05d27
Jean-Philippe Green <jeanphilippe.green@protonmail.com>
2022-06-13 07:55:19 +0200

    Use px instead of rem

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ import StateWrapper from "./store/StateWrapper";
 
 export default function App() {
   return (
-    <div className="flex flex-row flex-wrap items-stretch bg-white min-h-[calc(100vh-2rem)]">
+    <div className="flex flex-row flex-wrap items-stretch bg-white min-h-[calc(100vh-32px)]">
       <StateWrapper initialContext={initContext()}>
-        <div className="flex flex-col flex-grow w-96 max-h-[calc(100vh-2rem)]">
+        <div className="flex flex-col flex-grow w-96 max-h-[calc(100vh-32px)]">
           <EventSelection />
         </div>
         <div
-          className="flex-grow flex-shrink-0 min-h-[calc(100vh-2rem)] min-w-[calc(100%-24rem)]"
+          className="flex-grow flex-shrink-0 min-h-[calc(100vh-32px)] min-w-[calc(100%-384px)]"
           data-testid="calendar-wrapper"
         >
           <Calendar />
