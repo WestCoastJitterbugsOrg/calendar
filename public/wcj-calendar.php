65b7ec8195f271a3ec0d229d7a5270b6c6bcef9f
Jean-Philippe Green <jeanphilippe.green@protonmail.com>
2022-08-21 20:00:49 +0200

    Update php plugin version

--- public/wcj-calendar.php
+++ public/wcj-calendar.php
@@ -9,7 +9,7 @@
  * Plugin Name:       WCJ Calendar
  * Plugin URI:        https://github.com/WestCoastJitterbugsOrg/Personalized-Calendar
  * Description:       Filterable calendar using data from cogwork
- * Version:           1.5.0-alpha1
+ * Version:           1.4.3
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Jean-Philippe Green
