1dcd3f618eae75115f13f77499713ddf1784b0a0
Jean-Philippe Green <jeanphilippe.green@protonmail.com>
2023-02-05 11:37:58 +0100

    Simplify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,65 +8,19 @@
  * @type {WpConfig}
  */
 // @ts-ignore
+const { merge } = require('webpack-merge');
+// @ts-ignore
 const wpDefaults = require('@wordpress/scripts/config/webpack.config');
 const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
-
-const rules = wpDefaults.module?.rules ?? [];
-// Look for the css-loader from the wp defaults and modify it to output better css module class names
-for (const rule of rules) {
-	if (rule === '...') {
-		continue;
-	}
-	const ruleTest = rule.test;
-	if (!(ruleTest instanceof RegExp)) {
-		continue;
-	}
-	const suffixTest = ['.scss', '.css', '.sass'].some((suffix) =>
-		ruleTest.test(suffix)
-	);
-	if (!suffixTest) {
-		continue;
-	}
-
-	/** @type {{ loader: string; options: { modules: { localIdentName: string } };}[]}*/
-	// @ts-ignore
-	const ruleUse = rule.use;
-
-	const cssLoader = ruleUse.find(
-		(use) =>
-			use.loader === require.resolve('css-loader') ||
-			use.loader === 'css-loader'
-	);
-
-	if (!cssLoader) {
-		continue;
-	}
-	cssLoader.options = {
-		...cssLoader.options,
-		modules: {
-			...cssLoader.options.modules,
-			localIdentName: '[path][name]__[local]--[hash:base64:5]',
-		},
-	};
-}
+const path = require('path');
 
 /** @type {WpConfig} */
-const config = {
-	...wpDefaults,
-	externals: {
-		react: 'React',
-		'react-dom': 'ReactDOM',
-	},
-	module: {
-		...wpDefaults.module,
-		rules,
-	},
+const config = merge(wpDefaults, {
+	mode: 'development',
 	resolve: {
 		extensions: ['.js', '.jsx', '.ts', '.tsx'],
-		plugins: [
-			new TsconfigPathsPlugin({}),
-		],
-	},
-};
+		plugins: [new TsconfigPathsPlugin({})],
+	}
+});
 
 module.exports = config;
