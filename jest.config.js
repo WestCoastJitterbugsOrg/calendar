fc13b84bc586c47d0fb0d899810bd0a039416ae8
Jean-Philippe Green <jeanphilippe.green@protonmail.com>
2023-02-20 22:50:05 +0100

    Fix jest tests

--- jest.config.js
+++ jest.config.js
@@ -13,8 +13,10 @@ module.exports = {
 			'<rootDir>/test/__mocks__/fileMock.ts',
 		'\\.(css|scss|pcss)$': '<rootDir>/test/__mocks__/styleMock.ts',
 		...wpPreset.moduleNameMapper,
-		...pathsToModuleNameMapper(compilerOptions.paths, {
-			prefix: '<rootDir>/',
-		}),
+		...(compilerOptions.paths
+			? pathsToModuleNameMapper(compilerOptions.paths, {
+					prefix: '<rootDir>/',
+			  })
+			: {}),
 	},
 };
