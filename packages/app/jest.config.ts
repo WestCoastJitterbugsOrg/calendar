3b555c587398dfb97fa254bccf23cf1eb29d1cf4
Jean-Philippe Green <jean.philippe.green@brilliantfuture.se>
2023-08-21 22:17:33 +0200

    Get most tests passing

--- packages/app/jest.config.ts
+++ packages/app/jest.config.ts
@@ -1,18 +1,13 @@
-// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
-// which contains the path mapping (ie the `compilerOptions.paths` option):
 import { compilerOptions } from './tsconfig.json';
 import type { Config } from 'jest';
-// jest.config.js
 import { pathsToModuleNameMapper } from 'ts-jest';
 
 const moduleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, {
 	prefix: '<rootDir>/',
 });
 
-const config: Config = {
-	transform: {
-		'^.+\\.tsx?$': 'ts-jest',
-	},
+export default {
+	preset: 'ts-jest',
 	testRegex: '(/test/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
 	testPathIgnorePatterns: ['dist', 'build'],
 	testEnvironment: 'jsdom',
@@ -22,8 +17,7 @@ const config: Config = {
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
 			'<rootDir>/test/__mocks__/fileMock.ts',
 		'\\.(css|scss|pcss)$': '<rootDir>/test/__mocks__/styleMock.ts',
+		'^(src/.*)$': '<rootDir>/$1',
 		...moduleNameMapper,
 	},
-};
-
-export default config;
+} satisfies Config;
